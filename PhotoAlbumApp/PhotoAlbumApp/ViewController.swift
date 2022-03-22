1040685937e69ac1add5018c50ec37574aff49f7
Damagucci-Juice <jsr9874@gmail.com>
2022-03-22 12:12:06 +0900

    [Feat] CollectionView 생성 및 구현

--- PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
+++ PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    private let colors = ColorFactory.generateRandom(count: 40)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,5 +9,17 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = colors[indexPath.item]
+        return cell
+    }
 }
 
