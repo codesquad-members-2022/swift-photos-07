79882992cad08ba2246c9f77cd66a4edf06d67f8
Damagucci-Juice <jsr9874@gmail.com>
2022-03-22 11:03:06 +0900

    [Move] ColorRange 열거형 분리

--- PhotoAlbumApp/PhotoAlbumApp/Model/Color.swift
+++ PhotoAlbumApp/PhotoAlbumApp/Model/Color.swift
@@ -1,13 +1,7 @@
 import Foundation
 
-enum ColorRange {
-    static let lower = 0.0
-    static let upper = 255.0
-    static let defaultAlpha = 1.0
-}
-
 struct Color {
-    
+
     static func generateRandom() -> (Double, Double, Double, Double) {
         let red = Double.random(in: ColorRange.lower...ColorRange.upper)
         let green = Double.random(in: ColorRange.lower...ColorRange.upper)
