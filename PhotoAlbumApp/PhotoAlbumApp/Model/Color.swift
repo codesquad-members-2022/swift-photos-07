597bb672df691508ee249876a63403d17aac295b
Damagucci-Juice <jsr9874@gmail.com>
2022-03-22 12:07:37 +0900

    [Feat] Color 구조체 생성 및 구현

--- PhotoAlbumApp/PhotoAlbumApp/Model/Color.swift
+++ PhotoAlbumApp/PhotoAlbumApp/Model/Color.swift
@@ -1,13 +1,8 @@
 import Foundation
 
 struct Color {
-
-    static func generateRandom() -> (Double, Double, Double, Double) {
-        let red = Double.random(in: ColorRange.lower...ColorRange.upper)
-        let green = Double.random(in: ColorRange.lower...ColorRange.upper)
-        let blue = Double.random(in: ColorRange.lower...ColorRange.upper)
-        let alpha = ColorRange.defaultAlpha
-        
-        return (red, green, blue, alpha)
-    }
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Double
 }
