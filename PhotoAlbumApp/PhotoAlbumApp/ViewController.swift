f9e5056c24bf702660a16261820c0766133d5d6e
Damagucci-Juice <jsr9874@gmail.com>
2022-03-23 16:38:15 +0900

    [Feat] PH Photo Library 권한 설정까지 완료

--- PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
+++ PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
     
@@ -6,8 +7,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let requiredAccessLevel: PHAccessLevel = .readWrite
+        PHPhotoLibrary.requestAuthorization(for: requiredAccessLevel) { authorizationStatus in
+            switch authorizationStatus {
+            case .limited:
+                print("limited authorization granted")
+            case .authorized:
+                print("authorization granted")
+            default:
+                //FIXME: Implement handling for all authorizationStatus
+                print("Unimplemented")
+            }
+        }
+        
         configureViewController()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: nil)
+    }
+    
+    private func setPhotoLibraryImage() {
+        let fetchOption = PHFetchOptions()
+        fetchOption.fetchLimit = 1
+        fetchOption.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        let fetchPhotos = PHAsset.fetchAssets(with: fetchOption)
+        if let photo = fetchPhotos.firstObject {
+            DispatchQueue.main.async {
+                ImageManager.shared.requestImage(from: photo, thumnailSize: CGSize(width: 80.0, height: 80.0)) { image in
+                // 가져온 이미지로 (image 파라미터) 하고싶은 행동
+                }
+           }
+        }
     }
     
 }
