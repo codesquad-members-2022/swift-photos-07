a0774ff7c4848210aa735ca90871170b1869bf3d
Damagucci-Juice <jsr9874@gmail.com>
2022-03-23 13:22:08 +0900

    [Feat] 우향 네비게이션 바 아이템을 추가하고 다음 뷰컨을 지정

--- PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
+++ PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
@@ -1,25 +1,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     private let colors = ColorFactory.generateRandom(count: 40)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(rightBarButtonItemTouched))
     }
-
-
+    
 }
 
 extension ViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return colors.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         cell.contentView.backgroundColor = colors[indexPath.item]
         return cell
     }
+    
 }
 
+extension ViewController {
+    
+    @objc func rightBarButtonItemTouched() {
+        let vc = PhotoViewController()
+        navigationController?.pushViewController(vc, animated: true)
+
+    }
+
+}
