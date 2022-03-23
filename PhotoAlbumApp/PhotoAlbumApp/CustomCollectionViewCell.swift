a45e9841428026d1e80c64f56ea3c31f25b470d8
Damagucci-Juice <jsr9874@gmail.com>
2022-03-23 22:53:59 +0900

    [Feat] CustomCollectionViewCell 생성 및 구현

---
+++ PhotoAlbumApp/PhotoAlbumApp/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  PhotoAlbumApp
+//
+//  Created by YEONGJIN JANG and SANGHYEOK KIm on 2022/03/23.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
